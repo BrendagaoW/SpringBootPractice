675fb44dd52df9d435e51d4f7d1ae85dff3933aa
Lifeng Gao <lfgao@thoughtworks.com>
2016-03-09 16:52:18 +0800

    Use Jetty instead of Tomcat.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,10 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
+import java.util.*;
 
 @RestController
 @EnableAutoConfiguration
@@ -17,6 +18,25 @@ public class Application {
     @RequestMapping("/now")
     String hehe() {
         return "现在时间：" + (new Date()).toLocaleString();
+    }
+
+    @RequestMapping("/info")
+    public Map<String, String> getInfo(@RequestParam String name) {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("name", name);
+        return map;
+    }
+
+    @RequestMapping("/list")
+    public List<Map<String, String>> getList() {
+        List<Map<String, String>> list = new ArrayList();
+        Map<String, String> map = null;
+        for (int i = 1; i <= 5; i++) {
+            map = new HashMap<String, String>();
+            map.put("name", "Shanhy-" + i);
+            list.add(map);
+        }
+        return list;
     }
 
     public static void main(String[] args) {
